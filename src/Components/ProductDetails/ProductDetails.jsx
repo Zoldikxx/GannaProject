169308a9c67bef7bae65ba9a230364e8e0767f9f
Ziad Hashem <ziad.hashem@valleysoft-eg.com>
2024-02-27 02:08:59 +0200

    styles

--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import products from '../Products/Items/Items';
+import './ProductDetails.css';
 
 export default function ProductDetails() {
   const imageUrl = localStorage.getItem("imageUrl");
   const id = localStorage.getItem("productId");
 
   console.log("ProductId", id);
-  
+
   const [productDetails, setProductDetails] = useState(null);
 
   useEffect(() => {
     // Fetch product details based on productId (replace with actual data fetching)
     const fetchProductDetails = async () => {
       // Simulate fetching product details from API/database
-      const productslist =  products;
+      const productslist = products;
       const details = await products[id];
       console.log("Products", productslist)
       console.log("Details", details);
@@ -23,19 +24,23 @@ export default function ProductDetails() {
     fetchProductDetails();
   }, [id]);
 
-  console.log("ProductId",id);
+  console.log("ProductId", id);
 
   return (
     <div>
       {productDetails && (
-        <div className='d-flex justify-content-evenly'>
-          <div>
-          <img src={imageUrl} alt={productDetails.name} style={{ maxWidth: '100%', marginBottom: '1rem' }} />
+        <div className='container border border-2 border-black rounded-5 d-flex justify-content-evenly mt-4'>
+          <div className='w-50 p-5'>
+            <img src={imageUrl} alt={productDetails.name} style={{ maxWidth: '100%', marginBottom: '1rem' }}
+              className='border rounded-3' />
           </div>
-          <div>
-          <h2>{productDetails.name}</h2>
-          <p className='my-5'>{productDetails.description}</p>
-          <p>Price: {productDetails.price}</p>
+          <div className='d-flex justify-content-center align-content-center flex-column'>
+            <h2 className='font-monospace fw-bold'>{productDetails.name}</h2>
+            <p className='my-5'>{productDetails.description}</p>
+            <p className='fw-bold'>Price: <span className='p-2 text-danger'>{productDetails.price}</span></p>
+          </div>
+          <div className="d-flex justify-content-end align-items-end m-5">
+            <button className='cart-button py-2 px-5 rounded-pill bg-body-secondary text-success fw-bold'>Add to Cart</button>
           </div>
         </div>
       )}
--- src/Components/Products/Items/Items.js
+++ src/Components/Products/Items/Items.js
@@ -1,96 +1,111 @@
 import assets from '../../../projects_assets/flower-pot/products/productImages';
 
- const  products = [
-    {
-      id: 0,
-      name: 'Product 1',
-      description: 'This is the description for Product 1.',
-      imageUrl: assets.image0,
-    },
-    {
-      id: 1,
-      name: 'Product 2',
-      description: 'This is the description for Product 2.',
-      imageUrl: assets.image1,
-    },
-    {
-      id: 2,
-      name: 'Product 3',
-      description: 'This is the description for Product 3.',
-      imageUrl: assets.image2,
-    },
-    {
-      id: 3,
-      name: 'Product 4',
-      description: 'This is the description for Product 4.',
-      imageUrl: assets.image3,
-    },
-    {
-      id: 4,
-      name: 'Product 5',
-      description: 'This is the description for Product 5.',
-      imageUrl: assets.image4,
-    },
-    {
-      id: 5,
-      name: 'Product 6',
-      description: 'This is the description for Product 6.',
-      imageUrl: assets.image5,
-    },
-    {
-      id: 6,
-      name: 'Product 7',
-      description: 'This is the description for Product 7.',
-      imageUrl: assets.image6,
-    },
-    {
-      id: 7,
-      name: 'Product 8',
-      description: 'This is the description for Product 8.',
-      imageUrl: assets.image7,
-    },
-    {
-      id: 8,
-      name: 'Product 9',
-      description: 'This is the description for Product 9.',
-      imageUrl: assets.image8,
-    },
-    {
-      id: 9,
-      name: 'Product 10',
-      description: 'This is the description for Product 10.',
-      imageUrl: assets.image9,
-    },
-    {
-      id: 10,
-      name: 'Product 11',
-      description: 'This is the description for Product 11.',
-      imageUrl: assets.image10,
-    },
-    {
-      id: 11,
-      name: 'Product 12',
-      description: 'This is the description for Product 12.',
-      imageUrl: assets.image11,
-    },
-    {
-      id: 12,
-      name: 'Product 13',
-      description: 'This is the description for Product 13.',
-      imageUrl: assets.image12,
-    },
-    {
-      id: 13,
-      name: 'Product 14',
-      description: 'This is the description for Product 14.',
-      imageUrl: assets.image13,
-    },
-    {
-      id: 14,
-      name: 'Product 15',
-      description: 'This is the description for Product 15.',
-      imageUrl: assets.image14,
-    },
-  ];
+const products = [
+  {
+    id: 0,
+    name: 'Product 1',
+    description: 'This is the description for Product 1.',
+    imageUrl: assets.image0,
+    price: 10.99
+  },
+  {
+    id: 1,
+    name: 'Product 2',
+    description: 'This is the description for Product 2.',
+    imageUrl: assets.image1,
+    price: 15.99
+  },
+  {
+    id: 2,
+    name: 'Product 3',
+    description: 'This is the description for Product 3.',
+    imageUrl: assets.image2,
+    price: 20.99
+  },
+  {
+    id: 3,
+    name: 'Product 4',
+    description: 'This is the description for Product 4.',
+    imageUrl: assets.image3,
+    price: 25.99
+  },
+  {
+    id: 4,
+    name: 'Product 5',
+    description: 'This is the description for Product 5.',
+    imageUrl: assets.image4,
+    price: 30.99
+  },
+  {
+    id: 5,
+    name: 'Product 6',
+    description: 'This is the description for Product 6.',
+    imageUrl: assets.image5,
+    price: 35.99
+  },
+  {
+    id: 6,
+    name: 'Product 7',
+    description: 'This is the description for Product 7.',
+    imageUrl: assets.image6,
+    price: 40.99
+  },
+  {
+    id: 7,
+    name: 'Product 8',
+    description: 'This is the description for Product 8.',
+    imageUrl: assets.image7,
+    price: 45.99
+  },
+  {
+    id: 8,
+    name: 'Product 9',
+    description: 'This is the description for Product 9.',
+    imageUrl: assets.image8,
+    price: 50.99
+  },
+  {
+    id: 9,
+    name: 'Product 10',
+    description: 'This is the description for Product 10.',
+    imageUrl: assets.image9,
+    price: 55.99
+  },
+  {
+    id: 10,
+    name: 'Product 11',
+    description: 'This is the description for Product 11.',
+    imageUrl: assets.image10,
+    price: 60.99
+  },
+  {
+    id: 11,
+    name: 'Product 12',
+    description: 'This is the description for Product 12.',
+    imageUrl: assets.image11,
+    price: 65.99
+  },
+  {
+    id: 12,
+    name: 'Product 13',
+    description: 'This is the description for Product 13.',
+    imageUrl: assets.image12,
+    price: 70.99
+  },
+  {
+    id: 13,
+    name: 'Product 14',
+    description: 'This is the description for Product 14.',
+    imageUrl: assets.image13,
+    price: 75.99
+  },
+  {
+    id: 14,
+    name: 'Product 15',
+    description: 'This is the description for Product 15.',
+    imageUrl: assets.image14,
+    price: 80.99
+  },
+];
 
   export default products;
